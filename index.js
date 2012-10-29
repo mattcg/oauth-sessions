971f2c82b3c38d0914ead1462fbb3b44f259a766
Matthew Caruana Galizia <mattcg@gmail.com>
2012-10-29 00:55:37 +0000

    Add eventing support

--- index.js
+++ index.js
@@ -14,12 +14,22 @@ ProviderManager   = require('./lib/providerManager');
 
 /*jshint node:true */
 
+/**
+ * @param {string} options.logoutPath
+ * @param {string} options.providerFile
+ * @param {string} options.storeType
+ * @param {Object} options.storeOptions
+ */
 exports.create = function(options) {
 	'use strict';
-	var sessionManager, sessionNegotiator, storeManager, providerManager;
+	var sessionManager, sessionNegotiator, storeManager, providerManager, eventEmitter;
 
-	if (!options || !options.storeType) {
-		throw new TypeError();
+	if (!options) {
+		options = {};
+	}
+
+	if (!options.storeType) {
+		options.storeType = 'redis';
 	}
 
 	if (options.providerFile) {
@@ -29,9 +39,10 @@ exports.create = function(options) {
 		providerManager.setAll(options.providers);
 	}
 
+	eventEmitter      = new require('events').EventEmitter();
 	storeManager      = new (require('./lib/stores/' + options.storeType))(options.storeOptions);
-	sessionManager    = new SessionManager(storeManager);
+	sessionManager    = new SessionManager(storeManager, eventEmitter);
 	sessionNegotiator = new SessionNegotiator(providerManager, sessionManager, require('https'));
 
-	return new OAuthSessions(sessionManager, sessionNegotiator);
+	return new OAuthSessions(sessionManager, sessionNegotiator, eventEmitter);
 };
